d3853468e8483d748b0105c83b4029aed3298ee3
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-23 01:41:51 +0530

    Added routes

---
+++ src/db/index.js
@@ -0,0 +1,35 @@
+const db = [{
+  key: 'f0a9ccaee18a',
+  name: 'Tobias Cefalu',
+  email: 'tobias@gmail.com',
+  phone: '9278908738',
+  address: '239, North Lane, Street 11, New York',
+  fbHandle: 'tobias.cef',
+  twitterHandle: 'tobias.cef',
+  instaHandle: '',
+  linkedinHandle: 'tobias.cef'
+}, {
+  key: '1047fb1e4cac',
+  name: 'Josh Julien',
+  email: 'josh@ecorp.com',
+  phone: '9217394928',
+  address: 'Apartment 20, Folk Street, Riverdale',
+  fbHandle: 'joshjulien88',
+  twitterHandle: '',
+  instaHandle: 'joshjulien88',
+  linkedinHandle: ''
+}, {
+  key: '204af9a2309e',
+  name: 'Hugo Haverland',
+  email: 'hugo@yahoo.ca',
+  phone: '9618291928',
+  address: '90, Homermill, Off Street 97, Toronto',
+  fbHandle: '',
+  twitterHandle: 'hugo_hl',
+  instaHandle: 'hugo_hl',
+  linkedinHandle: ''
+}];
+
+export {
+	db
+}
