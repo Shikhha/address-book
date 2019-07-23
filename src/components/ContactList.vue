9ccb842bbb8e8d0ceff3b6b62bce1cdddc6972db
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-23 22:07:01 +0530

    Contact Detail component

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div @click="$emit(getDetails)" class="contact-list-item row p-md-3 text-center text-md-justify">
+    <div
+      @click="$emit('getDetails')"
+      class="contact-list-item row p-md-3 text-center text-md-justify"
+    >
       <div class="contact-name-item col-12 col-md-4">{{contact.name}}</div>
       <div class="contact-email-item col-12 col-md-4">{{contact.email}}</div>
       <div class="contact-phone-item col-12 col-md-4">{{contact.phone}}</div>
